e89f7bc5eef3d3072ac285f2e3ab87e9d6392bb3
onkar2323 <onkarsetia23@gmail.com>
2022-10-18 12:53:43 +0530

    d

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../app/controllers/userController.js');
+const userController = require('../controllers/userController.js');
 
 router.get('/', (req,res)=>{
     res.send('working')
