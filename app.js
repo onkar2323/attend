1e8127df731ba11b8ec3410acab3a0b466328c20
onkar2323 <onkarsetia23@gmail.com>
2022-10-18 12:44:41 +0530

    hjd

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-import express from 'express';
-import morgan from 'morgan';
-import { connect } from 'mongoose';
-import path from 'path';
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const path = require('path')
 require('dotenv').config();
-import { urlencoded, json } from 'body-parser';
-import { verify } from 'jsonwebtoken';
-import { findById } from './models/userModel';
-import routes from './routes/userRoutes.js';
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const User = require('./models/userModel');
+const routes = require('./routes/userRoutes.js');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(urlencoded({ extended: false }));
-app.use(json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
-connect(process.env.MONGO_DB).then((result)=>
+mongoose.connect(process.env.MONGO_DB).then((result)=>
     app.listen(process.env.PORT || 5000, (req,res)=>{
         console.log(`mongo connected, listenning on port: ${PORT}`)
     }))
@@ -28,12 +28,12 @@ app.use(morgan('dev'))
 app.use(async (req, res, next) => {
     if (req.headers["x-access-token"]) {
      const accessToken = req.headers["x-access-token"];
-     const { userId, exp } = await verify(accessToken, process.env.JWT_SECRET);
+     const { userId, exp } = await jwt.verify(accessToken, process.env.JWT_SECRET);
      // Check if token has expired
      if (exp < Date.now().valueOf() / 1000) { 
       return res.status(401).json({ error: "JWT token has expired, please login to obtain a new one" });
      } 
-     res.locals.loggedInUser = await findById(userId); next(); 
+     res.locals.loggedInUser = await User.findById(userId); next(); 
     } else { 
      next(); 
     } 
