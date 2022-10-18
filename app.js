ad224722c34bcb196f8d11f66cb79148af99b308
onkar2323 <onkarsetia23@gmail.com>
2022-10-18 12:43:05 +0530

    hjd

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const path = require('path')
+import express from 'express';
+import morgan from 'morgan';
+import { connect } from 'mongoose';
+import path from 'path';
 require('dotenv').config();
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const User = require('./models/userModel');
-const routes = require('./routes/userRoutes.js');
+import { urlencoded, json } from 'body-parser';
+import { verify } from 'jsonwebtoken';
+import { findById } from './models/userModel';
+import routes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(urlencoded({ extended: false }));
+app.use(json());
 
 
-mongoose.connect(process.env.MONGO_DB).then((result)=>
+connect(process.env.MONGO_DB).then((result)=>
     app.listen(process.env.PORT || 5000, (req,res)=>{
         console.log(`mongo connected, listenning on port: ${PORT}`)
     }))
@@ -28,12 +28,12 @@ app.use(morgan('dev'))
 app.use(async (req, res, next) => {
     if (req.headers["x-access-token"]) {
      const accessToken = req.headers["x-access-token"];
-     const { userId, exp } = await jwt.verify(accessToken, process.env.JWT_SECRET);
+     const { userId, exp } = await verify(accessToken, process.env.JWT_SECRET);
      // Check if token has expired
      if (exp < Date.now().valueOf() / 1000) { 
       return res.status(401).json({ error: "JWT token has expired, please login to obtain a new one" });
      } 
-     res.locals.loggedInUser = await User.findById(userId); next(); 
+     res.locals.loggedInUser = await findById(userId); next(); 
     } else { 
      next(); 
     } 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,10 +5,7 @@ const userController = require('../controllers/userController.js');
 router.get('/', (req,res)=>{
     res.send('working')
 })
- 
-router.get('/', (req,res)=>{
-    return res.send('working')
-})
+
 router.post('/signup', userController.signup);
  
 router.post('/login', userController.login);
